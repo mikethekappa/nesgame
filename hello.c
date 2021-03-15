cc9241330bc19a634af018cfc5f941d59e44ae77
mikethekappa <44155416+mikethekappa@users.noreply.github.com>
2021-03-15 08:17:02 -0400

    final I think

--- hello.c
+++ hello.c
@@ -11,21 +11,28 @@ Finally, turn on the PPU to display video.
 #include "bcd.h"
 #include "vrambuf.c"
 
+#define TILE 0xc4
+#define ATTR 0
 // link the pattern table into CHR ROM
 //#link "chr_generic.s"
-
+const unsigned char metasprite[]={
+        0,      0,      TILE+0,   ATTR, 
+        0,      8,      TILE+1,   ATTR, 
+        8,      0,      TILE+2,   ATTR, 
+        8,      8,      TILE+3,   ATTR, 
+        128};
 // main function, run after console reset
 void main(void) {
   // set palette colors
   unsigned int x = 15;
   bool left = true;
-  pal_col(0,0x19);	// set screen to dark blue
-  pal_col(1,0x14);	// fuchsia
+  pal_col(0,0x00);	// set screen to dark blue
+  pal_col(1,0x00);	// fuchsia
   pal_col(2,0x30);	// grey
   pal_col(3,0x30);	// white
-  pal_col(17,0x05);
-  pal_col(18,0x05);
-  pal_col(19,0x02);
+  pal_col(17,0x30);
+  pal_col(18,0x2c);
+  pal_col(19,0x08);
 
   // write text to name table
   vram_adr(NTADR_A(1,1));		// set address
@@ -43,21 +50,34 @@ void main(void) {
   // infinite loop
   while (1) {
     char cur_oam = 0;
+    char oam_id = 0;
+    unsigned int i = 8;
+    cur_oam = oam_meta_spr(230, 92, cur_oam, metasprite);
+    while (i < 256){
+      cur_oam = oam_spr(i, 108, 0xd7, 0x0, cur_oam);
+      i += 8;
+    }
     if(left){
       x-= 1;
       cur_oam = oam_spr(x, 100, 0x3f, 0x40, cur_oam);
-      vrambuf_put(NTADR_A(1, 4), "On door", 7);
+      
     }
     else{
       x+=1;
       cur_oam = oam_spr(x, 100, 0x3f, 0x0, cur_oam);
-      vrambuf_put(NTADR_A(1, 4), "       ", 7);
+      
     }
     if (x < 8){
       left = false;
     }
     if (x > 238){
       left = true;
+    }
+    if (x > 230){
+      vrambuf_put(NTADR_A(1, 4), "On door", 7);
+    }
+    else {
+      vrambuf_put(NTADR_A(1, 4), "       ", 7);
     }
     vrambuf_flush();
   }
